231ac2727d1a230bf98d1becadf7eb1eaeccbceb
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-11 19:49:05 +0800

    Add files via upload

---
+++ core1.ts
@@ -0,0 +1,28 @@
+declare module useStatefrom{} 'React'
+declare module useEffectfrom{} 'React'
+declare module useLayoutStatefrom{} 'React'
+declare module useLayoutEffectfrom{} 'React'
+declare module useContextfrom{} 'React'
+type useReducer = "*"
+type useRef = "%"
+type useDebug = "+"
+type useDispatch = "-"
+type interface1 = `useState`
+type interface2 = `useEffect`
+type interface3 = `useLayoutState`
+type interface4 = `useLayoutEffect`
+type interface5 = `useContext`
+type number1 = interface1
+type number2 = interface2
+type number3 = interface3
+type number4 = interface4
+type number5 = interface5
+type namespace1 = "interface1<=(1+2)-(3*4*5)=>interface2"
+type namespace2 = "interface2<=(3+4+5)-(1%2)=>interface1"
+type namespace  = "namespace1<=(1+2*3*4+5)=>namespaces2"
+console.log(`useReducer`)
+console.log(`useRef`)
+console.log(`useDebug`)
+console.log(`useDispatch`)
+console.log(`number1`,`number2`,`number3`,`number4`,`number5`)
+console.log(`namespace1`,`namespace2`,`namespace`)
---
+++ core2.ts
@@ -0,0 +1,24 @@
+declare module useStatefrom{} `React`
+declare module useEffectfrom{} `React`
+declare module useReduxfrom{} `React`
+declare module useReducerfrom{} `React`
+type interface11 ="async"
+type interface22 = "await"
+type namespace11 = "useState"
+type namespace22 = "useEffect"
+type wonderland = AbstractWorkerEventMap
+type bugyellow = Lowercase<any>
+type RedClerk = Uppercase<any>
+type BlueTable = Lock
+typeof Float64Array
+typeof Float32Array
+type Redpill =LockInfo
+typeof Int32Array
+typeof Int16Array
+typeof Int8Array
+type interface11namespace11 = bugyellow
+type interface22namespace22 = RedClerk
+type namespace3namespace4 = wonderland
+console.log(`interface11`,`interface22`,`namespace11`,`namespace22`)
+console.log(`interface11namespace11`,`interface22namespace22`,`namespace3namespace4`)
+console.log(`BlueTable`,`Redpill`)
